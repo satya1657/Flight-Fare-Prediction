07f2ea834e2ec3e809d534ca96702a66c09cb643
satyaprakash <kumarsatyaprakash33@gmail.com>
2021-03-18 16:17:15 +0530

    second commit

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def predict():
         date_dep = request.form["Dep_Time"]
         Journey_day = int(pd.to_datetime(date_dep, format="%Y-%m-%dT%H:%M").day)
         Journey_month = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").month)
-        # print("Journey Date : ",Journey_day, Journey_month)
+        print("Journey Date : ",Journey_day, Journey_month)
 
         # Departure
         Dep_hour = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").hour)
